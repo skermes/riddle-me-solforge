4fec7c5cfd9bd8e22415708385867b9a5c692199
Sean Kermes <skermes@gmail.com>
2015-08-11 12:47:25 -0400

    Gulp task for copying image assets.

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,19 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    clean = require('gulp-clean');
 
+var deployDir = 'public';
+
+var imgDir = deployDir + '/img';
+var imgExts = ['jpg', 'png'];
+var imgDeploySrc = imgExts.map(function(ext) { return imgDir + '/**/*.' + ext; });
+var imgDevSrc = imgExts.map(function(ext) { return 'img/**/*.' + ext; });
+
+gulp.task('clean-images', function() {
+  return gulp.src(imgDeploySrc)
+             .pipe(clean());
+});
+
+gulp.task('images', ['clean-images'], function() {
+  return gulp.src(imgDevSrc)
+             .pipe(gulp.dest(imgDir));
+});
