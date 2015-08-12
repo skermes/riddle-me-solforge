7738c507815d185655dfba768c058284a08e984e
Sean Kermes <skermes@gmail.com>
2015-08-11 23:38:39 -0400

    Store card data in local storage with version tag.

--- js/card-dictionary-store.js
+++ js/card-dictionary-store.js
@@ -2,6 +2,11 @@ var _ = require('lodash');
 var Reflux = require('reflux');
 var cardDataActions = require('./actions').cardData;
 
+// I could stick this in a config file or something but it's just as easy for
+// me to change this as anything else.
+// Might as well keep this value in line with Solforge card releases.
+var cardDataVersion = '6.0';
+
 var _dictionaryState = {
   cardList: undefined,
   cardDict: undefined,
@@ -19,6 +24,11 @@ var loadDataFromServer = function() {
     if (xhr.readyState === 4)  { // Done
       var success = xhr.status < 400;
       if (success) {
+        var data = xhr.response;
+
+        localStorage.setItem('solforgeCardData', JSON.stringify(data));
+        localStorage.setItem('solforgeCardDataVersion', cardDataVersion);
+
         cardDataActions.loaded(xhr.response);
       } else {
         cardDataActions.failedToLoad();
@@ -28,6 +38,27 @@ var loadDataFromServer = function() {
 
   xhr.send();
 }
+
+var loadDataFromLocalStorage = function() {
+  if (!window.localStorage) {
+    return false;
+  }
+
+  var localVersion = localStorage.getItem('solforgeCardDataVersion');
+  if (localVersion === cardDataVersion) {
+    cardDataActions.loaded(JSON.parse(localStorage.getItem('solforgeCardData')));
+    return true;
+  } else {
+    return false;
+  }
+};
+
+var loadData = function() {
+  var loadedFromLocalStorage = loadDataFromLocalStorage();
+  if (!loadedFromLocalStorage) {
+    loadDataFromServer();
+  }
+};
 
 var CardDictionary = Reflux.createStore({
   listenables: cardDataActions,
@@ -66,6 +97,6 @@ var CardDictionary = Reflux.createStore({
   }
 });
 
-loadDataFromServer();
+loadData();
 
 module.exports = CardDictionary;
